5f257aeec948470a8e264c02a75603356c4335c2
Jon Eilers <jon.eilers@wwu.edu>
2016-02-28 19:47:55 -0800

    Update psuedocode_for_tinyjplace.py

--- psuedocode_for_tinyjplace.py
+++ psuedocode_for_tinyjplace.py
@@ -1,2 +1,9 @@
 class data(object):
   
+  def content(self):
+    '''creates a dictionary/list/data type for storing data'''
+    data = dictionary()
+    
+  def add_content(self, formated_data):
+    '''adds formated data to content'''
+    data.update(i
