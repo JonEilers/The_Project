5494a6bfe19b369a19fec2b25507d5017b7df35f
Jon Eilers <jon.eilers@wwu.edu>
2016-02-28 20:05:50 -0800

    Update psuedocode_for_tinyjplace.py

--- psuedocode_for_tinyjplace.py
+++ psuedocode_for_tinyjplace.py
@@ -6,4 +6,12 @@ class data(object):
     
   def add_content(self, formated_data):
     '''adds formated data to content'''
-    data.update(i
+    data.update(key:value)
+    
+  def format_raw_content(self ,raw_data_item):
+    '''takes a single data item from raw data and outputs that data item in a desirable data type'''
+    
+    def data.type1_to_data.type2(datatype1):
+      '''returns data type 2'''
+      for i in datatype1:
+        outputs data.type2
