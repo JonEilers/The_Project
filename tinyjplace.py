b7462ca635424ef2774ed2e66876cfb1fc48d46c
Jon Eilers <jon.eilers@wwu.edu>
2016-03-01 20:44:11 -0800

    Update tinyjplace.py

--- tinyjplace.py
+++ tinyjplace.py
@@ -17,54 +17,61 @@ class NewickTree(_JplaceRecordComponent):
     '''A class representing a phylogenetic tree'''
     
     def __init__(self):
-      '''dictionary contains newick tree information'''
         self._tree = {}
         
     def __str__(self):
         return self._content
         
-    def __branch_length__(self, initial_node = 0, leaf = none):
-        '''calculates the branch length given the starting node and ending node. default ending node is none, 
-        this finds the longest branch length and returns it.'''
-    
-    def internal_edge(self):
-        '''parses a Newick tree and returns all the internal edges stores it as a dictionary. 
-        
-        key: edge number
-        values: edges or nodes it is connected to
-        '''
-        
-    def leaf((self):
-        '''parses a Newick tree and returns all leafs, storing them in a dictionary
-        
-        key: edge number
-        values: edge or node it is connected to
-        '''
-    
+    @property
     def _content(self):
         """Return the newick tree as a dictionary with key as node and values as everything else?.
         
         :returns: dict
         """
         
-    def add_tree_edge(self, tree_node):
+    def add_tree_edge(self, tree_edge):
         """
-        for parsing a tree and storing desirable values
+        Add an edge to the :class: tinyjplace.NewickTree instance.
         
-        Adds node/edge and its info to the JplaceTree instance. This function can
+        Adds edge and its info to the JplaceTree instance. This function can
         be called more than once. Each time the function is called the :class: tinyjplace.NewickTree
-        dictionary is extended by the key/value pair provided.
-        :param tree_node: dict key/value representing part of a newick formatted tree.
+        dictionary is extended by the edge key/value pair provided.
+        
+        :param tree_edge: dict key/value representing part of a newick formatted tree.
         """
         
-    def format_tree_node(self, node_data):
-        """iterates over the newick tree found in the jplace file, extracts a single node and formats it as 
-        a key/value dictionary pair. Each key contains information connecting it to distal and proximal nodes. 
-        These are used when writing out the
-        :class: tinyjplace.JplaceRecord over several nodes.
+    def format_tree_edge(self, edge_data):
+        """Takes a tree edge and formats it as a key/value dictionary pair.
+        The value contains information connecting it to distal and proximal nodes. 
         
-        :param node_data: information specific to that node.
+        :class: tinyjplace.JplaceRecord over several edges.
+        
+        :param edge_data: information specific to that edge.
         """ 
+        
+        def __branch_length__(self, initial_edge = 0, leaf = none):
+        '''calculates the branch length given the starting edge and ending edge. default ending edge is none, 
+        this finds the longest branch length and returns it.
+        
+        :param initial_edge: first edge
+               leaf: last edge '''
+    
+    def internal_edge(self):
+        '''parses a Newick tree
+        
+        returns: all the internal edges stores it as a dictionary. 
+        
+        key: edge number
+        values: edges or nodes it is connected to
+        '''
+        
+    def leaf(self):
+        '''parses a Newick tree and returns all leaf
+        returns: dictionary
+        
+        key: edge number
+        values: edge or node it is connected to
+        '''
     
 class PlacementRecord(object):
     '''A class representing phylogenetic tree placements'''
