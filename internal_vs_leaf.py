9dd7610b0595801b518357eb664e38b033a7ae3e
Jon Eilers <jon.eilers@wwu.edu>
2016-03-03 14:09:18 -0800

    Update internal_vs_leaf.py

--- internal_vs_leaf.py
+++ internal_vs_leaf.py
@@ -23,8 +23,6 @@ with open("COG0001.aligned.jplace") as json_data:
     # i is integer index of each element in list
     # v is value (string) at each index
     for i,v in enumerate(branches):
-        if '{' in v:
-            edgeCount += 1
         if "|" in v:
             leafCount += 1
             edgeNum = int(v.split('{')[1].split('}')[0])
@@ -32,7 +30,8 @@ with open("COG0001.aligned.jplace") as json_data:
         elif v != '':
             internal_edgeNum = int(v.split('{')[1].split('}')[0])
             internalEdges.append(internal_edgeNum)
-
+            
+    jplace_handle = data
     placements = jplace_handle['placements'] #placement key in json
     for placement in placements: #it works, but I am guessing there is a more efficient way to dig deep into a dictionary
         placement_edge = placement.values()[0][0][2]
