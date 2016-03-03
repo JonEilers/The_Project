614c2e1b81594d52b5482fe2154f137be61176e5
shelbyLanda <shelbylanda@gmail.com>
2016-03-02 21:09:07 -0800

    Update TreeParse.py

--- TreeParse.py
+++ TreeParse.py
@@ -4,18 +4,31 @@
 #Date :
 
 import json
+import re
 
 with open("COG0001.aligned.jplace") as json_data:
 
     data = json.load(json_data)
     tree = data['tree']
-    branches = tree.split(",")
-    internalCount = 0
-    for i in branches:
+    branches = re.split('[, ( )]' , tree)
+    
+    #arbitrary length to test ability of loops to do the right thing
+    arbLen = 50
+    #small subsect of larger tree
+    smallBranches = branches[0:arbLen]
+    
+    #initialize leafCount variable 
+    leafCount = 0
+    
+    # | is only present in tip labels
+    for i in smallBranches:
+       # if len(i) == 0:
+        #    del branches[i]
         if "|" in i:
-            internalCount+=1
-    print len(branches)
-
-
-
-##unicode strings??
+            #increment leafCount by 1
+            leafCount+=1
+    #does leafCount work with smallBranches length??
+    print leafCount
+    #check branches with leafCount
+    print branches[0:arbLen]
+    
