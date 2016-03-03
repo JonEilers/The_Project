a0ce8c7b439507c73deb2b2da60437a6cce4e997
Jon Eilers <jon.eilers@wwu.edu>
2016-03-03 13:32:31 -0800

    Create Placement_edge_parse.py

---
+++ Placement_edge_parse.py
@@ -0,0 +1,19 @@
+test_internal = [3366,1485,3351,1482,1767,1767,1814,1479,1767,1767]
+test_external = [1479,1479,1479,1477,1483]
+
+internal_count = 0
+external_count = 0
+with open('COG0001.aligned.jplace') as jplace_file:
+    jplace_handle = json.load(jplace_file)
+    placements = jplace_handle['placements']
+    for placement in placements:
+        placement_values = placement.values()
+        placement_fields = placement_values[0]
+        placement_field_values = placement_fields[0]
+        placement_edge = placement_field_values[2]
+        if placement_edge in test_internal:
+            internal_count += 1
+        elif placement_edge in test_external:
+            external_count += 1
+    print(internal_count)
+    print(external_count)
